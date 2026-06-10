a6c3c4224341141d53e45166aad2f8e42bc2ec5f
maketavimas <maketavimas@gmail.com>
2026-06-10 12:46:03 +0300

    Added email download modal

---
+++ script.js
@@ -0,0 +1,27 @@
+const DOWNLOAD_URL =
+"https://github.com/user-attachments/files/28760473/bandymui.txt";
+
+const downloadBtn = document.getElementById("downloadBtn");
+const modal = document.getElementById("modal");
+const confirmBtn = document.getElementById("confirmBtn");
+const cancelBtn = document.getElementById("cancelBtn");
+
+downloadBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+cancelBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+confirmBtn.addEventListener("click", () => {
+
+    const email = document.getElementById("email").value;
+
+    if (!email) {
+        alert("Please enter your email.");
+        return;
+    }
+
+    window.location.href = DOWNLOAD_URL;
+});
